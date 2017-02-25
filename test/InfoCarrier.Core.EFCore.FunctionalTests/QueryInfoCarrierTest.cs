72aec32973cb4d8ca5c60854fc17a083a0709aa1
Alexander Zabluda <alexander.zabluda@onoff-group.de>
2017-02-25 16:57:44 +0100

    Revert "Skip 'Take_with_single_select_many' test"
    This reverts commit c863b232f83452f3cb41a0e26df74dee39cea832.

--- test/InfoCarrier.Core.EFCore.FunctionalTests/QueryInfoCarrierTest.cs
+++ test/InfoCarrier.Core.EFCore.FunctionalTests/QueryInfoCarrierTest.cs
@@ -45,11 +45,5 @@
         {
             base.Where_subquery_expression_same_parametername();
         }
-
-        [Fact(Skip = "https://github.com/6bee/aqua-core/issues/6#issuecomment-281114757")]
-        public override void Take_with_single_select_many()
-        {
-            base.Take_with_single_select_many();
-        }
     }
 }
