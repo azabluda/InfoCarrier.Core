d4eb3e66c6b6ae80ffa65e3cfb199953acf273bf
Alexander Zabluda <alexander.zabluda@onoff-group.de>
2017-03-06 16:59:53 +0100

    Add InheritanceInfoCarrierTest

---
+++ test/InfoCarrier.Core.EFCore.FunctionalTests/InheritanceInfoCarrierFixture.cs
@@ -0,0 +1,40 @@
+﻿namespace InfoCarrier.Core.EFCore.FunctionalTests
+{
+    using System;
+    using Client;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Metadata.Conventions.Internal;
+    using Microsoft.EntityFrameworkCore.Specification.Tests;
+    using Microsoft.EntityFrameworkCore.Specification.Tests.TestModels.Inheritance;
+    using Microsoft.Extensions.DependencyInjection;
+
+    public class InheritanceInfoCarrierFixture : InheritanceFixtureBase
+    {
+        private static readonly string StoreName = nameof(InheritanceInfoCarrierTest);
+        private readonly Func<InheritanceContext> inMemoryDbContextFactory;
+
+        public InheritanceInfoCarrierFixture()
+        {
+            var optionsBuilder = new DbContextOptionsBuilder();
+            optionsBuilder.UseInMemoryDatabase(StoreName);
+            var modelBuilder = new ModelBuilder(new CoreConventionSetBuilder().CreateConventionSet());
+            this.OnModelCreating(modelBuilder);
+            optionsBuilder.UseModel(modelBuilder.Model);
+            this.inMemoryDbContextFactory = () => new InheritanceContext(optionsBuilder.Options);
+
+            using (var context = this.inMemoryDbContextFactory())
+            {
+                this.SeedData(context);
+            }
+        }
+
+        public override InheritanceContext CreateContext()
+                => new InheritanceContext(new DbContextOptionsBuilder()
+                    .UseInfoCarrierBackend(new TestInfoCarrierBackend(this.inMemoryDbContextFactory, true))
+                    .UseInternalServiceProvider(
+                        new ServiceCollection()
+                            .AddEntityFrameworkInfoCarrierBackend()
+                            .AddSingleton(TestInfoCarrierModelSource.GetFactory(this.OnModelCreating))
+                            .BuildServiceProvider()).Options);
+    }
+}
