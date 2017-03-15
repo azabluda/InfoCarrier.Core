ea0cdf65fc8ae228fddf994f0f38ce8b021c7056
Alexander Zabluda <alexander.zabluda@onoff-group.de>
2017-03-15 16:47:11 +0100

    Add 'ComplexNavigationsQueryInfoCarrierTest' class with some very bad tests causing StackOverflow

---
+++ test/InfoCarrier.Core.EFCore.FunctionalTests/ComplexNavigationsQueryInfoCarrierTest.cs
@@ -0,0 +1,12 @@
+﻿namespace InfoCarrier.Core.EFCore.FunctionalTests
+{
+    using Microsoft.EntityFrameworkCore.Specification.Tests;
+
+    public class ComplexNavigationsQueryInfoCarrierTest : ComplexNavigationsQueryTestBase<TestStore, ComplexNavigationsQueryInfoCarrierFixture>
+    {
+        public ComplexNavigationsQueryInfoCarrierTest(ComplexNavigationsQueryInfoCarrierFixture fixture)
+            : base(fixture)
+        {
+        }
+    }
+}
