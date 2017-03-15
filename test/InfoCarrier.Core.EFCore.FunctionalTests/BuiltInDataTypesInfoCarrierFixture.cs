e3b43821ff6a03cea0f9b4c08c567609d09689d5
Alexander Zabluda <alexander.zabluda@onoff-group.de>
2017-03-15 16:06:57 +0100

    Add 'BuiltInDataTypesInfoCarrierTest' class with a few failing tests
    - Can_insert_and_read_back_all_nullable_data_types_with_values_set_to_non_null
    - Can_perform_query_with_max_length
    - Can_query_using_any_data_type
    - Can_query_using_any_nullable_data_type

---
+++ test/InfoCarrier.Core.EFCore.FunctionalTests/BuiltInDataTypesInfoCarrierFixture.cs
@@ -0,0 +1,38 @@
+﻿namespace InfoCarrier.Core.EFCore.FunctionalTests
+{
+    using System;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Infrastructure;
+    using Microsoft.EntityFrameworkCore.Specification.Tests;
+    using Microsoft.EntityFrameworkCore.Storage.Internal;
+
+    public class BuiltInDataTypesInfoCarrierFixture : BuiltInDataTypesFixtureBase
+    {
+        private readonly InfoCarrierInMemoryTestHelper<DbContext> helper;
+
+        public BuiltInDataTypesInfoCarrierFixture()
+        {
+            this.helper = InfoCarrierInMemoryTestHelper.Create(
+                this.OnModelCreating,
+                (opt, _) => new DbContext(opt));
+        }
+
+        public override bool SupportsBinaryKeys
+            => false;
+
+        public override DateTime DefaultDateTime
+            => default(DateTime);
+
+        public override DbContext CreateContext()
+            => this.helper.CreateInfoCarrierContext();
+
+        public override void Dispose()
+        {
+            using (var context = this.helper.CreateInMemoryContext())
+            {
+                var storeSource = context.GetService<IInMemoryStoreSource>();
+                storeSource.GetGlobalStore().Clear();
+            }
+        }
+    }
+}
---
+++ test/InfoCarrier.Core.EFCore.FunctionalTests/BuiltInDataTypesInfoCarrierTest.cs
@@ -0,0 +1,19 @@
+﻿namespace InfoCarrier.Core.EFCore.FunctionalTests
+{
+    using Microsoft.EntityFrameworkCore.Specification.Tests;
+    using Xunit;
+
+    public class BuiltInDataTypesInfoCarrierTest : BuiltInDataTypesTestBase<BuiltInDataTypesInfoCarrierFixture>
+    {
+        public BuiltInDataTypesInfoCarrierTest(BuiltInDataTypesInfoCarrierFixture fixture)
+            : base(fixture)
+        {
+        }
+
+        [Fact]
+        public virtual void Can_perform_query_with_ansi_strings()
+        {
+            this.Can_perform_query_with_ansi_strings(supportsAnsi: false);
+        }
+    }
+}
