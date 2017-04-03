a876fdc882e77ad497a49ab3edd1f2e1406979f6
Alexander Zabluda <alexander.zabluda@onoff-group.de>
2017-04-03 13:16:51 +0200

    Add test class 'DatabindingInfoCarrierTest'
    The following tests are currently failing:
    - Entity_added_to_context_is_added_to_navigation_property_binding_list
    - Entity_added_to_navigation_property_binding_list_is_added_to_context_after_DetectChanges
    - Entity_removed_from_navigation_property_binding_list_is_removed_from_nav_property_but_not_marked_Deleted
    - LocalView_is_initialized_with_entities_from_the_context

---
+++ test/InfoCarrier.Core.EFCore.FunctionalTests/DatabindingInfoCarrierTest.cs
@@ -0,0 +1,12 @@
+﻿namespace InfoCarrier.Core.EFCore.FunctionalTests
+{
+    using Microsoft.EntityFrameworkCore.Specification.Tests;
+
+    public class DatabindingInfoCarrierTest : DatabindingTestBase<TestStore, F1InfoCarrierFixture>
+    {
+        public DatabindingInfoCarrierTest(F1InfoCarrierFixture fixture)
+            : base(fixture)
+        {
+        }
+    }
+}
