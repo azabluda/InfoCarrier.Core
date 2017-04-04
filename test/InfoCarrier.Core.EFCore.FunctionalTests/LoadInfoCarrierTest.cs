07bdc70a188d8eb7afba7d5ed05ff44d02f776f3
Alexander Zabluda <alexander.zabluda@onoff-group.de>
2017-04-04 17:52:16 +0200

    Add test class 'LoadInfoCarrierTest'

---
+++ test/InfoCarrier.Core.EFCore.FunctionalTests/LoadInfoCarrierTest.cs
@@ -0,0 +1,32 @@
+﻿namespace InfoCarrier.Core.EFCore.FunctionalTests
+{
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Specification.Tests;
+
+    public class LoadInfoCarrierTest
+        : LoadTestBase<TestStore, LoadInfoCarrierTest.LoadInfoCarrierFixture>
+    {
+        public LoadInfoCarrierTest(LoadInfoCarrierFixture fixture)
+            : base(fixture)
+        {
+        }
+
+        public class LoadInfoCarrierFixture : LoadFixtureBase
+        {
+            private readonly InfoCarrierInMemoryTestHelper<LoadContext> helper;
+
+            public LoadInfoCarrierFixture()
+            {
+                this.helper = InfoCarrierInMemoryTestHelper.Create(
+                    this.OnModelCreating,
+                    (opt, _) => new LoadContext(opt));
+            }
+
+            public override DbContext CreateContext(TestStore testStore)
+                => this.helper.CreateInfoCarrierContext();
+
+            public override TestStore CreateTestStore()
+                => this.helper.CreateTestStore(this.Seed);
+        }
+    }
+}
