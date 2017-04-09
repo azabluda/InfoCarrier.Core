ec9cdd8bbc24b270e4890ccdf524319942a6e5cd
Alexander Zabluda <alexander.zabluda@onoff-group.de>
2017-04-09 16:44:32 +0200

    Add test class 'NotificationEntitiesInfoCarrierTest'

---
+++ test/InfoCarrier.Core.EFCore.FunctionalTests/NotificationEntitiesInfoCarrierTest.cs
@@ -0,0 +1,32 @@
+﻿namespace InfoCarrier.Core.EFCore.FunctionalTests
+{
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Specification.Tests;
+
+    public class NotificationEntitiesInfoCarrierTest
+        : NotificationEntitiesTestBase<TestStore, NotificationEntitiesInfoCarrierTest.NotificationEntitiesInfoCarrierFixture>
+    {
+        public NotificationEntitiesInfoCarrierTest(NotificationEntitiesInfoCarrierFixture fixture)
+            : base(fixture)
+        {
+        }
+
+        public class NotificationEntitiesInfoCarrierFixture : NotificationEntitiesFixtureBase
+        {
+            private readonly InfoCarrierInMemoryTestHelper<DbContext> helper;
+
+            public NotificationEntitiesInfoCarrierFixture()
+            {
+                this.helper = InfoCarrierInMemoryTestHelper.Create(
+                    this.OnModelCreating,
+                    (opt, _) => new DbContext(opt));
+            }
+
+            public override DbContext CreateContext()
+                => this.helper.CreateInfoCarrierContext();
+
+            public override TestStore CreateTestStore()
+                => this.helper.CreateTestStore(_ => this.EnsureCreated());
+        }
+    }
+}
