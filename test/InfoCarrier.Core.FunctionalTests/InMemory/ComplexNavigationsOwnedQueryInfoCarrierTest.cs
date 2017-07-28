6eadfb96f9d32b684f30c1d9f0057e5e83341d4c
Alexander Zabluda <alexander.zabluda@onoff-group.de>
2017-07-28 15:25:28 +0200

    Add ComplexNavigationsOwnedQueryInfoCarrierTest

---
+++ test/InfoCarrier.Core.FunctionalTests/InMemory/ComplexNavigationsOwnedQueryInfoCarrierTest.cs
@@ -0,0 +1,19 @@
+﻿namespace InfoCarrier.Core.FunctionalTests.InMemory
+{
+    using Microsoft.EntityFrameworkCore.Specification.Tests;
+    using Xunit;
+
+    public class ComplexNavigationsOwnedQueryInfoCarrierTest : ComplexNavigationsOwnedQueryTestBase<TestStoreBase, ComplexNavigationsOwnedQueryInfoCarrierFixture>
+    {
+        public ComplexNavigationsOwnedQueryInfoCarrierTest(ComplexNavigationsOwnedQueryInfoCarrierFixture fixture)
+            : base(fixture)
+        {
+        }
+
+        [Fact(Skip = "issue #4311 (from ComplexNavigationsOwnedQueryInMemoryTest)")]
+        public override void Nested_group_join_with_take()
+        {
+            base.Nested_group_join_with_take();
+        }
+    }
+}
