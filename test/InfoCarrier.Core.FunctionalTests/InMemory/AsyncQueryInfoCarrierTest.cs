ad256eb1bd7eb6ac2249cf8776c941a3ce8c3a3d
Alexander Zabluda <alexander.zabluda@onoff-group.de>
2017-07-28 13:10:00 +0200

    Skip tests which EFC team also marked as skipped

--- test/InfoCarrier.Core.FunctionalTests/InMemory/AsyncQueryInfoCarrierTest.cs
+++ test/InfoCarrier.Core.FunctionalTests/InMemory/AsyncQueryInfoCarrierTest.cs
@@ -1,12 +1,20 @@
 ﻿namespace InfoCarrier.Core.FunctionalTests.InMemory
 {
+    using System.Threading.Tasks;
     using Microsoft.EntityFrameworkCore.Specification.Tests;
+    using Xunit;
 
     public class AsyncQueryInfoCarrierTest : AsyncQueryTestBase<NorthwindQueryInfoCarrierFixture>
     {
         public AsyncQueryInfoCarrierTest(NorthwindQueryInfoCarrierFixture fixture)
             : base(fixture)
         {
+        }
+
+        [Fact(Skip = "Not valid for in-memory (from AsyncQueryInMemoryTest)")]
+        public override Task ToList_on_nav_in_projection_is_async()
+        {
+            return base.ToList_on_nav_in_projection_is_async();
         }
     }
 }
--- test/InfoCarrier.Core.FunctionalTests/InMemory/ComplexNavigationsQueryInfoCarrierTest.cs
+++ test/InfoCarrier.Core.FunctionalTests/InMemory/ComplexNavigationsQueryInfoCarrierTest.cs
@@ -1,12 +1,19 @@
 ﻿namespace InfoCarrier.Core.FunctionalTests.InMemory
 {
     using Microsoft.EntityFrameworkCore.Specification.Tests;
+    using Xunit;
 
     public class ComplexNavigationsQueryInfoCarrierTest : ComplexNavigationsQueryTestBase<TestStoreBase, ComplexNavigationsQueryInfoCarrierFixture>
     {
         public ComplexNavigationsQueryInfoCarrierTest(ComplexNavigationsQueryInfoCarrierFixture fixture)
             : base(fixture)
         {
+        }
+
+        [Fact(Skip = "issue #4311 (from ComplexNavigationsQueryInMemoryTest)")]
+        public override void Nested_group_join_with_take()
+        {
+            base.Nested_group_join_with_take();
         }
     }
 }
