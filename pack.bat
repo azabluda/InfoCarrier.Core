698523e262669ead70e7a88d573146de2432e388
Alexander Zabluda <alexander.zabluda@onoff-group.de>
2017-08-02 14:54:12 +0200

    Re-activate unit test execution in build and CI scripts

--- pack.bat
+++ pack.bat
@@ -2,5 +2,5 @@ for /f "delims=" %%i in ('tools\GitVersion.CommandLine\tools\GitVersion.exe /sho
 
 clean ^
   && dotnet restore ^
-  && echo DISABLE!!! dotnet test test\InfoCarrier.Core.FunctionalTests\InfoCarrier.Core.FunctionalTests.csproj ^
+  && dotnet test test\InfoCarrier.Core.FunctionalTests\InfoCarrier.Core.FunctionalTests.csproj ^
   && dotnet pack src\InfoCarrier.Core\InfoCarrier.Core.csproj --output "..\..\artifacts" --configuration Debug --include-symbols
