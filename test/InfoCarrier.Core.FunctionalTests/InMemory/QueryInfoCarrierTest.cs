56c05481645ade35c1236f7719403aeb361a5651
Alexander Zabluda <alexander.zabluda@onoff-group.de>
2017-08-24 17:49:14 +0200

    'Take_with_single_select_many' test no longer misbehaves on .NET Core 2.0

--- test/InfoCarrier.Core.FunctionalTests/InMemory/QueryInfoCarrierTest.cs
+++ test/InfoCarrier.Core.FunctionalTests/InMemory/QueryInfoCarrierTest.cs
@@ -1,10 +1,6 @@
 ﻿namespace InfoCarrier.Core.FunctionalTests.InMemory
 {
-    using System;
-    using System.Linq;
-    using Microsoft.EntityFrameworkCore;
     using Microsoft.EntityFrameworkCore.Query;
-    using Microsoft.EntityFrameworkCore.TestModels.Northwind;
     using Xunit;
 
     public class QueryInfoCarrierTest : QueryTestBase<NorthwindQueryInfoCarrierFixture>
@@ -30,36 +26,6 @@
         public override void Client_Join_select_many()
         {
             base.Client_Join_select_many();
-        }
-
-        [Fact]
-        public override void Take_with_single_select_many()
-        {
-            // UGLY: this is a complete copy-n-paste of the original test.
-            // Workarounds an unexplainable failure when running the test with netcoreapp1.0.
-            AssertQuery<Customer, Order>((cs, os) =>
-                (from c in cs
-                    from o in os
-                    orderby c.CustomerID, o.OrderID
-                    select new { c, o })
-                .Take(1)
-                .Cast<object>()
-                .Single());
-
-            void AssertQuery<TItem1, TItem2>(
-                Func<IQueryable<TItem1>, IQueryable<TItem2>, object> query,
-                bool assertOrder = false)
-                where TItem1 : class
-                where TItem2 : class
-            {
-                using (var context = this.CreateContext())
-                {
-                    TestHelpers.AssertResults(
-                        new[] { query(NorthwindData.Set<TItem1>(), NorthwindData.Set<TItem2>()) },
-                        new[] { query(context.Set<TItem1>(), context.Set<TItem2>()) },
-                        assertOrder);
-                }
-            }
         }
 
         [Fact(Skip = "Revisit after https://github.com/aspnet/EntityFrameworkCore/issues/9301")]
