efe161fafe722dbb72c2cd6ba1bd813811c8f4b6
Alexander Zabluda <alexander.zabluda@onoff-group.de>
2017-10-20 12:45:01 +0200

    Add quick and dirty WcfSample.Android application. We have to wait for https://github.com/dotnet/sdk/issues/491 (as per http://www.natemcmaster.com/blog/2017/03/09/vs2015-to-vs2017-upgrade/) until we get better support for Xamarin.Android projects in dotnet/sdk. For now the project is excluded from the build. Enthusiasts may reactivate it in VS - Build - Configuration Manager.

---
+++ sample/WcfSample.Android/MainActivity.cs
@@ -0,0 +1,109 @@
+﻿// Copyright (c) on/off it-solutions gmbh. All rights reserved.
+// Licensed under the MIT license. See license.txt file in the project root for license information.
+
+namespace WcfSample
+{
+    using System;
+    using System.Linq;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Android.Content;
+    using global::Android.App;
+    using global::Android.OS;
+    using global::Android.Widget;
+    using InfoCarrier.Core.Client;
+    using InfoCarrier.Core.Common;
+    using Microsoft.EntityFrameworkCore;
+
+    [Activity(Label = "WcfSample.Android", MainLauncher = true, Icon = "@mipmap/icon")]
+    public class MainActivity : Activity
+    {
+        private const string PropServerUrl = @"serverUrl";
+
+        private readonly ISharedPreferences sharedPreferences =
+            Application.Context.GetSharedPreferences(Application.Context.PackageName, FileCreationMode.Private);
+
+        private TextView editServerUrl;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            this.SetContentView(Resource.Layout.Main);
+
+            // Get our button from the layout resource,
+            // and attach an event to it
+            Button button = this.FindViewById<Button>(Resource.Id.myButton);
+
+            button.Click += this.Button_Click;
+
+            this.editServerUrl = this.FindViewById<TextView>(Resource.Id.editServerUrl);
+            this.editServerUrl.Text = this.sharedPreferences.GetString(PropServerUrl, this.editServerUrl.Text);
+        }
+
+        private async void Button_Click(object sender, EventArgs e)
+        {
+            var consoleTextView = this.FindViewById<TextView>(Resource.Id.textConsole);
+            void ConsoleWriteLine(string line)
+            {
+                consoleTextView.Text += $"{line}\n";
+            }
+
+            try
+            {
+                consoleTextView.Text = string.Empty;
+
+                string serverUrl = this.editServerUrl.Text;
+                var optionsBuilder = new DbContextOptionsBuilder();
+                optionsBuilder.UseInfoCarrierBackend(new WcfBackendXamarinImpl(serverUrl));
+                var options = optionsBuilder.Options;
+
+                using (var contextEdit = this.sharedPreferences.Edit())
+                {
+                    contextEdit.PutString(PropServerUrl, serverUrl);
+                    contextEdit.Commit();
+                }
+
+                // Select and update
+                using (var context = new BloggingContext(options))
+                {
+                    Post myBlogPost = await (
+                        from blog in context.Blogs
+                        from post in blog.Posts
+                        join owner in context.Users on blog.OwnerId equals owner.Id
+                        where owner.Name == "hi-its-me"
+                        where post.Title == "my-blog-post"
+                        select post).SingleAsync();
+
+                    ConsoleWriteLine($@"Blog post '{myBlogPost.Title}' is retrieved.");
+
+                    myBlogPost.CreationDate = DateTime.Now;
+                    await context.SaveChangesAsync();
+                    ConsoleWriteLine($@"CreationDate is set to '{myBlogPost.CreationDate}'.");
+                }
+            }
+            catch (Exception exception)
+            {
+                ConsoleWriteLine(exception.ToString());
+            }
+        }
+
+        // Xamarin.Android implementation of asynchronous WCF method calls is broken in a very
+        // weird way (throws no exception but just never executes the continuation after await).
+        // We have to simulate asynchrony by calling synchronous methods on the thread pool.
+        private class WcfBackendXamarinImpl : WcfBackendImpl
+        {
+            public WcfBackendXamarinImpl(string serverUrl)
+                : base(serverUrl)
+            {
+            }
+
+            public override async Task<QueryDataResult> QueryDataAsync(QueryDataRequest request, DbContext dbContext, CancellationToken cancellationToken)
+                => await Task.Run(() => this.QueryData(request, dbContext), cancellationToken);
+
+            public override async Task<SaveChangesResult> SaveChangesAsync(SaveChangesRequest request, CancellationToken cancellationToken)
+                => await Task.Run(() => this.SaveChanges(request), cancellationToken);
+        }
+    }
+}
