e913a990793eee49d5a0b7294be0020a893231a9
Alexander Zabluda <alexander.zabluda@onoff-group.de>
2018-04-24 18:21:53 +0200

    Stack overflow in Throws_on_concurrent_query_* tests

--- test/InfoCarrier.Core.FunctionalTests/InMemory/Query/AsyncSimpleQueryInfoCarrierTest.cs
+++ test/InfoCarrier.Core.FunctionalTests/InMemory/Query/AsyncSimpleQueryInfoCarrierTest.cs
@@ -3,14 +3,101 @@
 
 namespace InfoCarrier.Core.FunctionalTests.InMemory.Query
 {
+    using System;
+    using System.Linq;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Internal;
     using Microsoft.EntityFrameworkCore.Query;
+    using Microsoft.EntityFrameworkCore.TestModels.Northwind;
     using Microsoft.EntityFrameworkCore.TestUtilities;
+    using Xunit;
 
     public class AsyncSimpleQueryInfoCarrierTest : AsyncSimpleQueryTestBase<NorthwindQueryInfoCarrierFixture<NoopModelCustomizer>>
     {
         public AsyncSimpleQueryInfoCarrierTest(NorthwindQueryInfoCarrierFixture<NoopModelCustomizer> fixture)
             : base(fixture)
         {
+        }
+
+        public override async Task Throws_on_concurrent_query_list()
+        {
+            // UGLY: have to copy the whole test and add .AsEnumerable() to avoid stack overflow
+            // https://github.com/aspnet/EntityFrameworkCore/blob/2.1.0-preview2-final/src/EFCore.Specification.Tests/Query/AsyncSimpleQueryTestBase.cs#L3374
+            using (var context = this.CreateContext())
+            {
+                using (var synchronizationEvent = new ManualResetEventSlim(false))
+                {
+                    using (var blockingSemaphore = new SemaphoreSlim(0))
+                    {
+                        var blockingTask = Task.Run(
+                            () =>
+                                context.Customers.AsEnumerable().Select(
+                                    c => Process(c, synchronizationEvent, blockingSemaphore)).ToList());
+
+                        var throwingTask = Task.Run(
+                            async () =>
+                            {
+                                synchronizationEvent.Wait();
+
+                                Assert.Equal(
+                                    CoreStrings.ConcurrentMethodInvocation,
+                                    (await Assert.ThrowsAsync<InvalidOperationException>(
+                                        () => context.Customers.ToListAsync())).Message);
+                            });
+
+                        await throwingTask;
+
+                        blockingSemaphore.Release(1);
+
+                        await blockingTask;
+                    }
+                }
+            }
+        }
+
+        public override async Task Throws_on_concurrent_query_first()
+        {
+            // UGLY: have to copy the whole test and add .AsEnumerable() to avoid stack overflow
+            // https://github.com/aspnet/EntityFrameworkCore/blob/2.1.0-preview2-final/src/EFCore.Specification.Tests/Query/AsyncSimpleQueryTestBase.cs#L3409
+            using (var context = this.CreateContext())
+            {
+                using (var synchronizationEvent = new ManualResetEventSlim(false))
+                {
+                    using (var blockingSemaphore = new SemaphoreSlim(0))
+                    {
+                        var blockingTask = Task.Run(
+                            () =>
+                                context.Customers.AsEnumerable().Select(
+                                    c => Process(c, synchronizationEvent, blockingSemaphore)).ToList());
+
+                        var throwingTask = Task.Run(
+                            async () =>
+                            {
+                                synchronizationEvent.Wait();
+                                Assert.Equal(
+                                    CoreStrings.ConcurrentMethodInvocation,
+                                    (await Assert.ThrowsAsync<InvalidOperationException>(
+                                        () => context.Customers.FirstAsync())).Message);
+                            });
+
+                        await throwingTask;
+
+                        blockingSemaphore.Release(1);
+
+                        await blockingTask;
+                    }
+                }
+            }
+        }
+
+        private static Customer Process(Customer c, ManualResetEventSlim e, SemaphoreSlim s)
+        {
+            e.Set();
+            s.Wait();
+            s.Release(1);
+            return c;
         }
     }
 }
--- test/InfoCarrier.Core.FunctionalTests/InMemory/Query/SimpleQueryInfoCarrierTest.cs
+++ test/InfoCarrier.Core.FunctionalTests/InMemory/Query/SimpleQueryInfoCarrierTest.cs
@@ -3,8 +3,15 @@
 
 namespace InfoCarrier.Core.FunctionalTests.InMemory.Query
 {
+    using System;
+    using System.Linq;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Microsoft.EntityFrameworkCore.Internal;
     using Microsoft.EntityFrameworkCore.Query;
+    using Microsoft.EntityFrameworkCore.TestModels.Northwind;
     using Microsoft.EntityFrameworkCore.TestUtilities;
+    using Xunit;
     using Xunit.Abstractions;
 
     public class SimpleQueryInfoCarrierTest : SimpleQueryTestBase<NorthwindQueryInfoCarrierFixture<NoopModelCustomizer>>
@@ -14,6 +21,84 @@ namespace InfoCarrier.Core.FunctionalTests.InMemory.Query
             ITestOutputHelper testOutputHelper)
             : base(fixture)
         {
+        }
+
+        public override void Throws_on_concurrent_query_list()
+        {
+            // UGLY: have to copy the whole test and add .AsEnumerable() to avoid stack overflow
+            // https://github.com/aspnet/EntityFrameworkCore/blob/2.1.0-preview2-final/src/EFCore.Specification.Tests/Query/SimpleQueryTestBase.cs#L2597
+            using (var context = this.CreateContext())
+            {
+                context.Database.EnsureCreated();
+
+                using (var synchronizationEvent = new ManualResetEventSlim(false))
+                {
+                    using (var blockingSemaphore = new SemaphoreSlim(0))
+                    {
+                        var blockingTask = Task.Run(() =>
+                            context.Customers.AsEnumerable().Select(
+                                c => Process(c, synchronizationEvent, blockingSemaphore)).ToList());
+
+                        var throwingTask = Task.Run(() =>
+                        {
+                            synchronizationEvent.Wait();
+                            Assert.Equal(
+                                CoreStrings.ConcurrentMethodInvocation,
+                                Assert.Throws<InvalidOperationException>(
+                                    () => context.Customers.ToList()).Message);
+                        });
+
+                        throwingTask.Wait();
+
+                        blockingSemaphore.Release(1);
+
+                        blockingTask.Wait();
+                    }
+                }
+            }
+        }
+
+        public override void Throws_on_concurrent_query_first()
+        {
+            // UGLY: have to copy the whole test and add .AsEnumerable() to avoid stack overflow
+            // https://github.com/aspnet/EntityFrameworkCore/blob/2.1.0-preview2-final/src/EFCore.Specification.Tests/Query/SimpleQueryTestBase.cs#LL2632
+            using (var context = this.CreateContext())
+            {
+                context.Database.EnsureCreated();
+
+                using (var synchronizationEvent = new ManualResetEventSlim(false))
+                {
+                    using (var blockingSemaphore = new SemaphoreSlim(0))
+                    {
+                        var blockingTask = Task.Run(() =>
+                            context.Customers.AsEnumerable().Select(
+                                c => Process(c, synchronizationEvent, blockingSemaphore)).ToList());
+
+                        var throwingTask = Task.Run(() =>
+                        {
+                            synchronizationEvent.Wait();
+                            Assert.Equal(
+                                CoreStrings.ConcurrentMethodInvocation,
+                                Assert.Throws<InvalidOperationException>(
+                                    () => context.Customers.First()).Message);
+                        });
+
+                        throwingTask.Wait();
+
+                        blockingSemaphore.Release(1);
+
+                        blockingTask.Wait();
+                    }
+                }
+            }
+        }
+
+        private static Customer Process(Customer c, ManualResetEventSlim e, SemaphoreSlim s)
+        {
+            e.Set();
+            s.Wait();
+            s.Release(1);
+            return c;
         }
     }
 }
