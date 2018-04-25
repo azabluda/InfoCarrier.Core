21432d2a8d52b3290c1ed2242257450f059cb17d
Alexander Zabluda <alexander.zabluda@onoff-group.de>
2018-04-25 12:12:40 +0200

    SimulateNetworkTransferJson

--- test/InfoCarrier.Core.FunctionalTests/TestUtilities/TestInfoCarrierBackend.cs
+++ test/InfoCarrier.Core.FunctionalTests/TestUtilities/TestInfoCarrierBackend.cs
@@ -4,11 +4,17 @@
 namespace InfoCarrier.Core.FunctionalTests.TestUtilities
 {
     using System;
+    using System.Collections.Generic;
+    using System.Linq;
     using System.Threading;
     using System.Threading.Tasks;
     using Client;
     using Common;
     using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Infrastructure;
+    using Microsoft.EntityFrameworkCore.Update;
+    using Newtonsoft.Json;
+    using Remote.Linq;
     using Server;
 
     public class TestInfoCarrierBackend : IInfoCarrierBackend, IDisposable
@@ -29,33 +35,33 @@ namespace InfoCarrier.Core.FunctionalTests.TestUtilities
 
         public QueryDataResult QueryData(QueryDataRequest request, DbContext dbContext)
         {
-            using (var helper = new QueryDataHelper(this.backendStore.CreateDbContext, request))
+            using (var helper = new QueryDataHelper(this.backendStore.CreateDbContext, SimulateNetworkTransferJson(request)))
             {
-                return helper.QueryData();
+                return SimulateNetworkTransferJson(helper.QueryData());
             }
         }
 
         public async Task<QueryDataResult> QueryDataAsync(QueryDataRequest request, DbContext dbContext, CancellationToken cancellationToken)
         {
-            using (var helper = new QueryDataHelper(this.backendStore.CreateDbContext, request))
+            using (var helper = new QueryDataHelper(this.backendStore.CreateDbContext, SimulateNetworkTransferJson(request)))
             {
-                return await helper.QueryDataAsync(cancellationToken);
+                return SimulateNetworkTransferJson(await helper.QueryDataAsync(cancellationToken));
             }
         }
 
         public SaveChangesResult SaveChanges(SaveChangesRequest request)
         {
-            using (var helper = new SaveChangesHelper(this.backendStore.CreateDbContext, request))
+            using (var helper = new SaveChangesHelper(this.backendStore.CreateDbContext, SimulateNetworkTransferJson(request)))
             {
-                return helper.SaveChanges();
+                return SimulateNetworkTransferJson(helper.SaveChanges());
             }
         }
 
         public async Task<SaveChangesResult> SaveChangesAsync(SaveChangesRequest request, CancellationToken cancellationToken)
         {
-            using (var helper = new SaveChangesHelper(this.backendStore.CreateDbContext, request))
+            using (var helper = new SaveChangesHelper(this.backendStore.CreateDbContext, SimulateNetworkTransferJson(request)))
             {
-                return await helper.SaveChangesAsync(cancellationToken);
+                return SimulateNetworkTransferJson(await helper.SaveChangesAsync(cancellationToken));
             }
         }
 
@@ -90,6 +96,18 @@ namespace InfoCarrier.Core.FunctionalTests.TestUtilities
         public void Dispose()
         {
             this.backendStore.Dispose();
+        }
+
+        private static T SimulateNetworkTransferJson<T>(T value)
+        {
+            if (value == null)
+            {
+                return default(T);
+            }
+
+            var serializerSettings = new JsonSerializerSettings().ConfigureRemoteLinq();
+            var json = JsonConvert.SerializeObject(value, serializerSettings);
+            return (T)JsonConvert.DeserializeObject(json, value.GetType(), serializerSettings);
         }
     }
 }
