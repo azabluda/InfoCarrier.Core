5a23226e3aace5b59f6ba216cb972ea5cb88c45d
Alexander Zabluda <alexander.zabluda@onoff-group.de>
2018-05-09 18:09:27 +0200

    Change port for ServiceStackSample to avoid conflicts with OpenSSL

--- sample/ServiceStackShared.cs
+++ sample/ServiceStackShared.cs
@@ -16,7 +16,7 @@ namespace InfoCarrierSample
     {
         public static string BaseAddress { get; } =
             Environment.GetEnvironmentVariable(@"ServiceStack__DefaultBaseAddress")
-                ?? @"https://localhost:1337/";
+                ?? @"https://localhost:11337/";
     }
 
     public abstract class HasData
