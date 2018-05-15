4492578d1bc8bb43f866112212b53a590643d71a
Alexander Zabluda <alexander.zabluda@onoff-group.de>
2018-05-15 18:58:25 +0200

    Convert coverage.xml to readable HTML format using ReportGenerator-4.0.0-alpha4 (netcoreapp2.0)

--- coverage.bat
+++ coverage.bat
@@ -3,3 +3,7 @@ dotnet test -f netcoreapp2.1 ^
     /p:CoverletOutputFormat=opencover ^
     /p:CoverletOutputDirectory="%~dp0TestResults" ^
     test\InfoCarrier.Core.FunctionalTests\InfoCarrier.Core.FunctionalTests.csproj
+
+dotnet tools\ReportGenerator\ReportGenerator.dll -reports:TestResults\coverage.xml -targetdir:TestResults\coverage
+
+start "" "TestResults\coverage\index.htm"
