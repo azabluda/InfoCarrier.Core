8ba76a698a4f45bf78962649af999423c8bb03b9
Alexander Zabluda <alexander.zabluda@onoff-group.de>
2018-12-11 18:13:00 +0100

    Even the old Throws_on_concurrent_query_ tests cause troubles now.
    Skip them for SimpleQueryInfoCarrierTest. Concurrency detection mechanism cannot be used there.

--- test/InfoCarrier.Core.FunctionalTests/InMemory/Query/AsyncSimpleQueryInfoCarrierTest.cs
+++ test/InfoCarrier.Core.FunctionalTests/InMemory/Query/AsyncSimpleQueryInfoCarrierTest.cs
@@ -3,51 +3,14 @@
 
 namespace InfoCarrier.Core.FunctionalTests.InMemory.Query
 {
-    using System;
-    using System.Threading.Tasks;
-    using Microsoft.EntityFrameworkCore;
-    using Microsoft.EntityFrameworkCore.Infrastructure;
-    using Microsoft.EntityFrameworkCore.Internal;
     using Microsoft.EntityFrameworkCore.Query;
     using Microsoft.EntityFrameworkCore.TestUtilities;
-    using Microsoft.Extensions.DependencyInjection;
-    using Xunit;
 
     public class AsyncSimpleQueryInfoCarrierTest : AsyncSimpleQueryTestBase<NorthwindQueryInfoCarrierFixture<NoopModelCustomizer>>
     {
         public AsyncSimpleQueryInfoCarrierTest(NorthwindQueryInfoCarrierFixture<NoopModelCustomizer> fixture)
             : base(fixture)
         {
-        }
-
-        public override async Task Throws_on_concurrent_query_list()
-        {
-            // Old implementation prior to
-            // https://github.com/aspnet/EntityFrameworkCore/commit/654dbcc408d4649a54d0ed7de5f1f06b64114f8b
-            using (var context = this.CreateContext())
-            {
-                ((IInfrastructure<IServiceProvider>)context).Instance.GetService<IConcurrencyDetector>().EnterCriticalSection();
-
-                Assert.Equal(
-                    CoreStrings.ConcurrentMethodInvocation,
-                    (await Assert.ThrowsAsync<InvalidOperationException>(
-                        async () => await context.Customers.ToListAsync())).Message);
-            }
-        }
-
-        public override async Task Throws_on_concurrent_query_first()
-        {
-            // Old implementation prior to
-            // https://github.com/aspnet/EntityFrameworkCore/commit/654dbcc408d4649a54d0ed7de5f1f06b64114f8b
-            using (var context = this.CreateContext())
-            {
-                ((IInfrastructure<IServiceProvider>)context).Instance.GetService<IConcurrencyDetector>().EnterCriticalSection();
-
-                Assert.Equal(
-                    CoreStrings.ConcurrentMethodInvocation,
-                    (await Assert.ThrowsAsync<InvalidOperationException>(
-                        async () => await context.Customers.FirstAsync())).Message);
-            }
         }
     }
 }
