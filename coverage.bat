85a853d0d87f47db4c237944309ac6db683d12c8
Alexander Zabluda <alexander.zabluda@onoff-group.de>
2019-01-15 14:17:53 +0100

    coverlet.msbuild 2.5.0

--- coverage.bat
+++ coverage.bat
@@ -1,7 +1,8 @@
 dotnet test -f netcoreapp2.1 ^
     /p:CollectCoverage=true ^
     /p:CoverletOutputFormat=opencover ^
-    /p:CoverletOutputDirectory="%~dp0TestResults" ^
+    /p:CoverletOutput="%~dp0TestResults\coverage.xml" ^
+    /p:Include="[InfoCarrier.Core]*" ^
     test\InfoCarrier.Core.FunctionalTests\InfoCarrier.Core.FunctionalTests.csproj
 
 dotnet tools\ReportGenerator\ReportGenerator.dll -reports:TestResults\coverage.xml -targetdir:TestResults\coverage
