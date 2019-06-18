23e8dc55deb899cbe0b2f3ba90bd68131bfa2c11
Alexander Zabluda <alexander.zabluda@onoff-group.de>
2019-06-18 14:05:19 +0200

    Add ByteArrayValueMapper standard value mapper to short circuit mapping of byte[] values to aqua-core (new built-in feature of 4.5.2) and thus prevent ArrayValueMapper and EnumerableValueMapper from getting into play.

---
+++ src/InfoCarrier.Core/Common/ValueMapping/StandardValueMappers.ByteArrayValueMapper.cs
@@ -0,0 +1,40 @@
+﻿// Copyright (c) on/off it-solutions gmbh. All rights reserved.
+// Licensed under the MIT license. See license.txt file in the project root for license information.
+
+namespace InfoCarrier.Core.Common.ValueMapping
+{
+    using Aqua.TypeSystem;
+
+    /// <summary>
+    /// Standard value mapper for arrays of bytes.
+    /// </summary>
+    public static partial class StandardValueMappers
+    {
+        private class ByteArrayValueMapper : IInfoCarrierValueMapper
+        {
+            public bool TryMapToDynamicObject(IMapToDynamicObjectContext context, out object mapped)
+            {
+                if (context.Object.GetType() == typeof(byte[]))
+                {
+                    mapped = context.Object;
+                    return true;
+                }
+
+                mapped = null;
+                return false;
+            }
+
+            public bool TryMapFromDynamicObject(IMapFromDynamicObjectContext context, out object obj)
+            {
+                if (context.Dto.Type?.ResolveType(context.TypeResolver) == typeof(byte[]))
+                {
+                    obj = context.Dto;
+                    return true;
+                }
+
+                obj = null;
+                return false;
+            }
+        }
+    }
+}
