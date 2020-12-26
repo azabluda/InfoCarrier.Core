889a455723c9fa527604b6529b5697455fec11fd
Alexander Zabluda <azabluda@gmail.com>
2020-12-26 15:01:00 +0100

    Fix WithConstructors tests. Remove insignificant assert on LoaderSetterCalled.

--- test/InfoCarrier.Core.FunctionalTests/InMemory/WithConstructorsInfoCarrierTest.cs
+++ test/InfoCarrier.Core.FunctionalTests/InMemory/WithConstructorsInfoCarrierTest.cs
@@ -8,6 +8,7 @@ namespace InfoCarrier.Core.FunctionalTests.InMemory
     using Microsoft.EntityFrameworkCore;
     using Microsoft.EntityFrameworkCore.Diagnostics;
     using Microsoft.EntityFrameworkCore.TestUtilities;
+    using Xunit;
 
     public class WithConstructorsInfoCarrierTest : WithConstructorsTestBase<WithConstructorsInfoCarrierTest.TestFixture>
     {
@@ -21,6 +22,64 @@ namespace InfoCarrier.Core.FunctionalTests.InMemory
             base.Query_and_update_using_constructors_with_property_parameters();
 
             this.Fixture.Reseed();
+        }
+
+        [ConditionalFact]
+        public override void Query_with_loader_injected_into_property_via_constructor_for_reference()
+        {
+            using (var context = this.CreateContext())
+            {
+                var post = context.Set<LazyPcPost>().OrderBy(e => e.Id).First();
+
+                // Assert on LoaderSetterCalled is insignificant. InfoCarrier.Core is setting service properties.
+                // Assert.False(post.LoaderSetterCalled);
+                Assert.NotNull(post.LazyPcBlog);
+                Assert.Contains(post, post.LazyPcBlog.LazyPcPosts);
+            }
+        }
+
+        [ConditionalFact]
+        public override void Query_with_loader_injected_into_property_via_constructor_for_collections()
+        {
+            using (var context = this.CreateContext())
+            {
+                var blog = context.Set<LazyPcBlog>().Single();
+
+                // Assert on LoaderSetterCalled is insignificant. InfoCarrier.Core is setting service properties.
+                // Assert.False(blog.LoaderSetterCalled);
+                Assert.Equal(2, blog.LazyPcPosts.Count());
+                Assert.Same(blog, blog.LazyPcPosts.First().LazyPcBlog);
+                Assert.Same(blog, blog.LazyPcPosts.Skip(1).First().LazyPcBlog);
+            }
+        }
+
+        [ConditionalFact]
+        public override void Query_with_loader_delegate_injected_into_property_via_constructor_for_reference()
+        {
+            using (var context = this.CreateContext())
+            {
+                var post = context.Set<LazyPcsPost>().OrderBy(e => e.Id).First();
+
+                // Assert on LoaderSetterCalled is insignificant. InfoCarrier.Core is setting service properties.
+                // Assert.False(post.LoaderSetterCalled);
+                Assert.NotNull(post.LazyPcsBlog);
+                Assert.Contains(post, post.LazyPcsBlog.LazyPcsPosts);
+            }
+        }
+
+        [ConditionalFact]
+        public override void Query_with_loader_delegate_injected_into_property_via_constructor_for_collections()
+        {
+            using (var context = this.CreateContext())
+            {
+                var blog = context.Set<LazyPcsBlog>().Single();
+
+                // Assert on LoaderSetterCalled is insignificant. InfoCarrier.Core is setting service properties.
+                // Assert.False(blog.LoaderSetterCalled);
+                Assert.Equal(2, blog.LazyPcsPosts.Count());
+                Assert.Same(blog, blog.LazyPcsPosts.First().LazyPcsBlog);
+                Assert.Same(blog, blog.LazyPcsPosts.Skip(1).First().LazyPcsBlog);
+            }
         }
 
         public class TestFixture : WithConstructorsFixtureBase
