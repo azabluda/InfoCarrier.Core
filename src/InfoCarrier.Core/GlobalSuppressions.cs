6c1a677d42c6cd9981202a8139358f1ea385e324
Alexander Zabluda <azabluda@gmail.com>
2020-12-27 15:27:40 +0100

    Suppress warning "EF1001:Internal EF Core API usage"
    InfoCarrier.Core provider heavily relies on EF.Core internals.

---
+++ src/InfoCarrier.Core/GlobalSuppressions.cs
@@ -0,0 +1,6 @@
+﻿// Copyright (c) on/off it-solutions gmbh. All rights reserved.
+// Licensed under the MIT license. See license.txt file in the project root for license information.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Usage", "EF1001:Internal EF Core API usage.", Justification = "InfoCarrier.Core provider heavily relies on EF.Core internals.")]
