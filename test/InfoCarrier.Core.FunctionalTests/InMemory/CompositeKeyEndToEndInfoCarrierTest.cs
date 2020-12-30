42e733eee1ee2e082e2a994fa33b505fec3bedb5
Alexander Zabluda <azabluda@gmail.com>
2020-12-30 19:33:43 +0100

    Update version, copyright, license information.

--- test/InfoCarrier.Core.FunctionalTests/InMemory/CompositeKeyEndToEndInfoCarrierTest.cs
+++ test/InfoCarrier.Core.FunctionalTests/InMemory/CompositeKeyEndToEndInfoCarrierTest.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) on/off it-solutions gmbh. All rights reserved.
+﻿// Copyright (c) Alexander Zabluda. All rights reserved.
 // Licensed under the MIT license. See license.txt file in the project root for license information.
 
 namespace InfoCarrier.Core.FunctionalTests.InMemory
